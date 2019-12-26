2f9d87221e164ffe605a7454f5e001c0ad0fdedf
gbs4ever <gbs4ever2@gmail.com>
2019-12-26 18:26:35 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById("create-task-form")
+  form.addEventListener('submit',onSubmit)
+})
+  
+ let  onSubmit = (event)=>{
+    event.preventDefault()
+    console.log(event)
+    addToDom()
+ 
+  }
+
+let addToDom = ()=>{
+  const newTaskDescription = document.getElementById("new-task-description")
+  const list = document.getElementById("tasks")
+  let input =newTaskDescription.value
+  let add =`<li>${input}</li> `
+  list.innerHTML = add  
+}
+// fuction to rewact when button hit
+// function to add data to array 
+// function to add elments to page ul with id tasks 
+// then add  at the  an li with the 
+
+
